ea0089e0ae7f71496a67e6d39016a6eb6fd5742a
spongycode <hkbettiah@gmail.com>
2021-06-09 14:55:28 +0530

    image text edit

---
+++ app/src/main/java/com/spongycode/blankspace/ui/main/ColorPickerAdapter.kt
@@ -0,0 +1,102 @@
+package com.spongycode.blankspace.ui.main
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.LayerDrawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.OvalShape
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.spongycode.blankspace.R
+import com.spongycode.blankspace.ui.main.ColorPickerAdapter.ViewHolder
+import java.util.*
+
+
+internal class ColorPickerAdapter internal constructor(private var context: Context, colorPickerColors: List<Int>) :
+        RecyclerView.Adapter<ViewHolder>() {
+    private var inflater: LayoutInflater
+    private val colorPickerColors: List<Int>
+    private var onColorPickerClickListener: ColorPickerAdapter.OnColorPickerClickListener? = null
+
+    internal constructor(context: Context) : this(context, ColorPickerAdapter.Companion.getDefaultColors(context)) {
+        this.context = context
+        inflater = LayoutInflater.from(context)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = inflater.inflate(R.layout.color_picker_item_list, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.colorPickerView.setBackgroundColor(colorPickerColors[position])
+    }
+
+    override fun getItemCount(): Int {
+        return colorPickerColors.size
+    }
+
+    private fun buildColorPickerView(view: View, colorCode: Int) {
+        view.visibility = View.VISIBLE
+        val biggerCircle = ShapeDrawable(OvalShape())
+        biggerCircle.intrinsicHeight = 20
+        biggerCircle.intrinsicWidth = 20
+        biggerCircle.bounds = Rect(0, 0, 20, 20)
+        biggerCircle.paint.color = colorCode
+        val smallerCircle = ShapeDrawable(OvalShape())
+        smallerCircle.intrinsicHeight = 5
+        smallerCircle.intrinsicWidth = 5
+        smallerCircle.bounds = Rect(0, 0, 5, 5)
+        smallerCircle.paint.color = Color.WHITE
+        smallerCircle.setPadding(10, 10, 10, 10)
+        val drawables = arrayOf<Drawable>(smallerCircle, biggerCircle)
+        val layerDrawable = LayerDrawable(drawables)
+        view.setBackgroundDrawable(layerDrawable)
+    }
+
+    fun setOnColorPickerClickListener(onColorPickerClickListener: ColorPickerAdapter.OnColorPickerClickListener?) {
+        this.onColorPickerClickListener = onColorPickerClickListener
+    }
+
+    internal inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var colorPickerView: View
+
+        init {
+            colorPickerView = itemView.findViewById(R.id.color_picker_view)
+            itemView.setOnClickListener { onColorPickerClickListener?.onColorPickerClickListener(colorPickerColors[adapterPosition]) }
+        }
+    }
+
+    interface OnColorPickerClickListener {
+        fun onColorPickerClickListener(colorCode: Int)
+    }
+
+    companion object {
+        fun getDefaultColors(context: Context?): List<Int> {
+            val colorPickerColors = ArrayList<Int>()
+            colorPickerColors.add(ContextCompat.getColor(context!!, R.color.blue_color_picker))
+            colorPickerColors.add(ContextCompat.getColor(context, R.color.brown_color_picker))
+            colorPickerColors.add(ContextCompat.getColor(context, R.color.green_color_picker))
+            colorPickerColors.add(ContextCompat.getColor(context, R.color.orange_color_picker))
+            colorPickerColors.add(ContextCompat.getColor(context, R.color.red_color_picker))
+            colorPickerColors.add(ContextCompat.getColor(context, R.color.black))
+            colorPickerColors.add(ContextCompat.getColor(context, R.color.red_orange_color_picker))
+            colorPickerColors.add(ContextCompat.getColor(context, R.color.sky_blue_color_picker))
+            colorPickerColors.add(ContextCompat.getColor(context, R.color.violet_color_picker))
+            colorPickerColors.add(ContextCompat.getColor(context, R.color.white))
+            colorPickerColors.add(ContextCompat.getColor(context, R.color.yellow_color_picker))
+            colorPickerColors.add(ContextCompat.getColor(context, R.color.yellow_green_color_picker))
+            return colorPickerColors
+        }
+    }
+
+    init {
+        inflater = LayoutInflater.from(context)
+        this.colorPickerColors = colorPickerColors
+    }
+}
--- app/src/main/java/com/spongycode/blankspace/ui/main/fragments/GenerateFragment.kt
+++ app/src/main/java/com/spongycode/blankspace/ui/main/fragments/GenerateFragment.kt
@@ -2,15 +2,93 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import com.spongycode.blankspace.R
+import com.spongycode.blankspace.databinding.FragmentGenerateBinding
+import com.spongycode.blankspace.ui.main.TextEditorDialogFragment
+import ja.burhanrashid52.photoeditor.OnPhotoEditorListener
+import ja.burhanrashid52.photoeditor.PhotoEditor
+import ja.burhanrashid52.photoeditor.PhotoEditorView
+import ja.burhanrashid52.photoeditor.TextStyleBuilder
+import ja.burhanrashid52.photoeditor.ViewType
 
+
+@Suppress("DEPRECATION")
 class GenerateFragment : Fragment() {
+
+    private var _binding: FragmentGenerateBinding? = null
+    private val binding get() = _binding!!
+
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_generate, container, false)
+        _binding = FragmentGenerateBinding.inflate(inflater, container, false)
+
+
+        val mPhotoEditorView: PhotoEditorView = binding.photoEditorView
+
+        mPhotoEditorView.source.setImageResource(R.drawable.croc)
+
+
+        val mTextRobotoTf = ResourcesCompat.getFont(requireContext(), R.font.opensans)
+        val mEmojiRobotoTf = ResourcesCompat.getFont(requireContext(), R.font.opensans)
+
+
+        val mPhotoEditor = PhotoEditor.Builder(requireContext(), mPhotoEditorView)
+                .setPinchTextScalable(true)
+                .setDefaultTextTypeface(mTextRobotoTf)
+                .setDefaultEmojiTypeface(mEmojiRobotoTf)
+                .build()
+
+
+        binding.memeUndo.setOnClickListener {
+            mPhotoEditor.undo()
+        }
+
+        binding.memeRedo.setOnClickListener {
+            mPhotoEditor.redo()
+        }
+
+
+        binding.memeAddText.setOnClickListener {
+            mPhotoEditor.addText("Hold to Edit", resources.getColor(R.color.purple_700))
+        }
+
+
+
+
+        mPhotoEditor.setOnPhotoEditorListener(object : OnPhotoEditorListener {
+
+            override fun onEditTextChangeListener(rootView: View?, text: String?, colorCode: Int) {
+                val textEditorDialogFragment = TextEditorDialogFragment.show(requireActivity() as AppCompatActivity, text!!, colorCode)
+                textEditorDialogFragment.setOnTextEditorListener { inputText, colorCode ->
+                    val styleBuilder = TextStyleBuilder()
+                    styleBuilder.withTextColor(colorCode)
+                    mPhotoEditor.editText(rootView!!, inputText, styleBuilder)
+                }
+            }
+
+            override fun onAddViewListener(viewType: ViewType?, numberOfAddedViews: Int) {
+                Unit
+            }
+
+            override fun onRemoveViewListener(viewType: ViewType?, numberOfAddedViews: Int) {
+                Unit
+            }
+
+            override fun onStartViewChangeListener(viewType: ViewType?) {
+                Unit
+            }
+
+            override fun onStopViewChangeListener(viewType: ViewType?) {
+                Unit
+            }
+        })
+
+        return binding.root
     }
 }
