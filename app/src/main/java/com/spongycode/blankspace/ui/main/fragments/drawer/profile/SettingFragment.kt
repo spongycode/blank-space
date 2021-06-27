256918b8c4463ebb30da425cd2af903e2b6d0cc8
spongycode <hkbettiah@gmail.com>
2021-06-27 20:30:12 +0530

    settings status

--- app/src/main/java/com/spongycode/blankspace/ui/main/fragments/drawer/profile/SettingFragment.kt
+++ app/src/main/java/com/spongycode/blankspace/ui/main/fragments/drawer/profile/SettingFragment.kt
@@ -8,6 +8,8 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
@@ -20,9 +22,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.google.android.gms.tasks.Continuation
 import com.google.android.gms.tasks.Task
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.UploadTask
@@ -40,16 +45,16 @@ import kotlinx.coroutines.withContext
 import java.io.ByteArrayOutputStream
 import java.util.*
 
-class SettingFragment: Fragment() {
+class SettingFragment : Fragment() {
 
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
     lateinit var username: EditText
+    lateinit var status: EditText
     lateinit var email: TextView
     lateinit var btnSubmit: Button
     lateinit var editProPic: TextView
     lateinit var ivProPic: ImageView
-    lateinit var progressBar: ProgressBar
     private var currImageUrl: String = userdata.afterLoginUserData.imageUrl
 
 
@@ -67,7 +72,8 @@ class SettingFragment: Fragment() {
         val toolbar: Toolbar = binding.toolSettings
         (activity as AppCompatActivity?)!!.setSupportActionBar(toolbar)
 
-        val navHostFragment = (activity as AppCompatActivity).supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
+        val navHostFragment =
+            (activity as AppCompatActivity).supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
         toolbar.setNavigationIcon(R.drawable.ic_nav_up)
@@ -81,19 +87,26 @@ class SettingFragment: Fragment() {
         ivProPic = binding.ivProfilePic
         Glide.with(this).load(userdata.afterLoginUserData.imageUrl).into(ivProPic)
 
-        progressBar = binding.progressImage
-        progressBar.visibility = GONE
-
-
         username = binding.textInputEditText
         username.setText(userdata.afterLoginUserData.username)
+        status = binding.textInputStatus
+        if (userdata.afterLoginUserData.status != "") {
+            status.setText(userdata.afterLoginUserData.status)
+        }
         email = binding.textInputEditText01
         email.text = userdata.afterLoginUserData.email
 
         btnSubmit = binding.submitButton
+        enableBtn(false)
+        Helper.buttonEffect(btnSubmit, "#C665F37D")
+
 
         btnSubmit.setOnClickListener {
-            updateProfile(newImageUrl = currImageUrl, newUsername = username.text.toString())
+            updateProfile(
+                newImageUrl = currImageUrl,
+                newUsername = username.text.toString(),
+                newStatus = status.text.toString()
+            )
         }
 
         editProPic = binding.editProfilePic
@@ -104,23 +117,82 @@ class SettingFragment: Fragment() {
             )
             startActivityForResult(gallery, pickImage)
         }
+
+
+        status.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                val state =
+                    (s.toString() != userdata.afterLoginUserData.status ||
+                            username.text.toString() != userdata.afterLoginUserData.username ||
+                            currImageUrl != userdata.afterLoginUserData.imageUrl)
+                enableBtn(state)
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // no use for now
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // no use for now
+            }
+        })
+
+        username.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                val state =
+                    (s.toString() != userdata.afterLoginUserData.username ||
+                            status.text.toString() != userdata.afterLoginUserData.status ||
+                            currImageUrl != userdata.afterLoginUserData.imageUrl)
+                enableBtn(state)
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // no use for now
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // no use for now
+            }
+        })
+
+
+
+
+
+
+
+
+
+
+
+
         return binding.root
     }
 
-    private fun updateProfile(newImageUrl: String, newUsername: String) {
+    private fun enableBtn(state: Boolean) {
+        btnSubmit.isEnabled = state
+        if (!state) {
+            btnSubmit.alpha = 0.5f
+        }else{
+            btnSubmit.alpha = 1f
+        }
+    }
+
+    private fun updateProfile(newImageUrl: String, newUsername: String, newStatus: String) {
         CoroutineScope(Dispatchers.IO).launch {
-            if (newImageUrl != userdata.afterLoginUserData.imageUrl) {
+            if (newImageUrl != userdata.afterLoginUserData.imageUrl || newStatus != userdata.afterLoginUserData.status) {
                 userdata.afterLoginUserData.imageUrl = newImageUrl
+                userdata.afterLoginUserData.status = newStatus
                 firestore.collection("users").document(userdata.afterLoginUserData.userId)
-                    .update("imageUrl", newImageUrl)
+                    .update("imageUrl", newImageUrl, "status", newStatus)
                     .addOnCompleteListener {
-                        Glide.with(requireActivity()).load(userdata.afterLoginUserData.imageUrl)
-                            .into(ivProPic)
-                        Toast.makeText(
-                            requireContext(),
-                            "Profile Pic Updated",
-                            Toast.LENGTH_SHORT
-                        ).show()
+//                        Glide.with(requireActivity()).load(userdata.afterLoginUserData.imageUrl)
+//                            .into(ivProPic)
+//                        Toast.makeText(
+//                            requireContext(),
+//                            "Profile Pic Updated",
+//                            Toast.LENGTH_SHORT
+//                        ).show()
                     }
                     .await()
             }
@@ -133,23 +205,33 @@ class SettingFragment: Fragment() {
                             .addOnCompleteListener {
                                 userdata.afterLoginUserData.username = newUsername
                                 username.setText(userdata.afterLoginUserData.username)
-                                Toast.makeText(
-                                    requireContext(),
-                                    "Username changed",
-                                    Toast.LENGTH_LONG
+                                Snackbar.make(
+                                    (activity as AppCompatActivity).findViewById(android.R.id.content),
+                                    "Profile Updated.",
+                                    Snackbar.LENGTH_SHORT
                                 ).show()
+                                enableBtn(false)
                             }
                     } else {
                         withContext(Dispatchers.Main) {
-                            Toast.makeText(
-                                requireContext(),
-                                "Pick unique username",
-                                Toast.LENGTH_LONG
+                            Snackbar.make(
+                                (activity as AppCompatActivity).findViewById(android.R.id.content),
+                                "Pick unique username.",
+                                Snackbar.LENGTH_SHORT
                             ).show()
                         }
                     }
                 } catch (e: Exception) {
 
+                }
+            } else {
+                withContext(Dispatchers.Main) {
+                    Snackbar.make(
+                        (activity as AppCompatActivity).findViewById(android.R.id.content),
+                        "Profile Updated.",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                    enableBtn(false)
                 }
             }
         }
@@ -159,7 +241,14 @@ class SettingFragment: Fragment() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK && requestCode == pickImage && data != null && data.data != null) {
-            progressBar.visibility = VISIBLE
+//            progressBar.visibility = VISIBLE
+            val circularProgressDrawable = CircularProgressDrawable(requireContext())
+            circularProgressDrawable.strokeWidth = 10f
+            circularProgressDrawable.centerRadius = 50f
+            circularProgressDrawable.start()
+            Glide.with(this).load("").placeholder(circularProgressDrawable).into(ivProPic)
+
+
             val imageUri: Uri = data.data!!
             val extension: String =
                 imageUri.toString().substring(imageUri.toString().lastIndexOf("."))
@@ -182,9 +271,10 @@ class SettingFragment: Fragment() {
             }).addOnSuccessListener { task ->
                 val downloadUri = task
                 currImageUrl = downloadUri.toString()
-                progressBar.visibility = GONE
+//                progressBar.visibility = GONE
                 try {
-                    Glide.with(this).load(currImageUrl).into(ivProPic)
+                    Glide.with(this).load(imageUri).placeholder(circularProgressDrawable).into(ivProPic)
+                    enableBtn(true)
                 } catch (e: Exception) {
                 }
             }
@@ -192,13 +282,16 @@ class SettingFragment: Fragment() {
     }
 
 
-
     private fun getImageByteArray(imageUri: Uri): Any {
         val originalBitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            val source = ImageDecoder.createSource((activity as AppCompatActivity).contentResolver, imageUri)
+            val source =
+                ImageDecoder.createSource((activity as AppCompatActivity).contentResolver, imageUri)
             ImageDecoder.decodeBitmap(source)
         } else {
-            MediaStore.Images.Media.getBitmap((activity as AppCompatActivity).contentResolver, imageUri)
+            MediaStore.Images.Media.getBitmap(
+                (activity as AppCompatActivity).contentResolver,
+                imageUri
+            )
         }
         val scaledBitmap = BitmapScaler.scaleToFitHeight(originalBitmap, 1000)
         val byteOutputStream = ByteArrayOutputStream()
